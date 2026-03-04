c20fb0db59de26fed71f7cb677c9b94685d5ca87
Samuel Laycock <samuel.paul.laycock@gmail.com>
2026-03-04 10:18:47 +0000

    fix: fix platform utility return types

--- src/utils.ts
+++ src/utils.ts
@@ -1,47 +1,54 @@
 // eslint-disable-next-line
-export type Platform = "web" | "ios" | "android" | "windows" | "macos" | "linux" | string;
+export type Platform =
+  | "web"
+  | "ios"
+  | "android"
+  | "windows"
+  | "macos"
+  | "linux"
+  | string;
 
 export function platform<T, D>(
-  options: Record<Platform, T | undefined>,
-  fallback?: D,
-): T | D | undefined {
+  options: Record<Platform, T>,
+  fallback?: D
+): T | D {
   for (const key in options) {
     if (__PLATFORM__ === key) {
-      return (options[key] as T) ?? fallback;
+      return ((options[key] as T) ?? fallback) as T;
     }
   }
 
-  return fallback;
+  return fallback as D;
 }
 
-export function web<T, D>(value: T, fallback?: D): T | D | undefined {
+export function web<T, D>(value: T, fallback?: D): T | D {
   return platform({ web: value }, fallback);
 }
 
-export function mobile<T, D>(value: T, fallback?: D): T | D | undefined {
+export function mobile<T, D>(value: T, fallback?: D): T | D {
   return platform({ ios: value, android: value }, fallback);
 }
 
-export function desktop<T, D>(value: T, fallback?: D): T | D | undefined {
+export function desktop<T, D>(value: T, fallback?: D): T | D {
   return platform({ windows: value, macos: value, linux: value }, fallback);
 }
 
-export function ios<T, D>(value: T, fallback?: D): T | D | undefined {
+export function ios<T, D>(value: T, fallback?: D): T | D {
   return platform({ ios: value }, fallback);
 }
 
-export function android<T, D>(value: T, fallback?: D): T | D | undefined {
+export function android<T, D>(value: T, fallback?: D): T | D {
   return platform({ android: value }, fallback);
 }
 
-export function windows<T, D>(value: T, fallback?: D): T | D | undefined {
+export function windows<T, D>(value: T, fallback?: D): T | D {
   return platform({ windows: value }, fallback);
 }
 
-export function macos<T, D>(value: T, fallback?: D): T | D | undefined {
+export function macos<T, D>(value: T, fallback?: D): T | D {
   return platform({ macos: value }, fallback);
 }
 
-export function linux<T, D>(value: T, fallback?: D): T | D | undefined {
+export function linux<T, D>(value: T, fallback?: D): T | D {
   return platform({ linux: value }, fallback);
 }
